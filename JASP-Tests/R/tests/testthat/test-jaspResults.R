39632811f378dd95ecc54caaf7af345c1c5703c1
TimKDJ <tim_jong@hotmail.com>
2019-07-09 11:26:39 +0200

    initial

---
+++ JASP-Tests/R/tests/testthat/test-jaspResults.R
@@ -0,0 +1,78 @@
+context("jaspResults")
+
+# tables
+# test_that("jaspTable sets citations", {
+#     table <- createJaspTable()
+#     table$addCitation(c("citation1", "citation2"))
+# })
+
+# addColumnInfo()
+# addColumns()
+# addFootnote()
+# addRows()
+# dependOn()
+# getError()
+# print()
+# printHtml()
+# setData()
+# setError()
+# setExpectedSize()
+# position
+# showSpecifiedColumnsOnly
+# status
+# title
+# transpose
+# transposeWithOvertitle
+
+# plots
+addCitation()
+addFootnote()
+dependOn()
+getError()
+print()
+printHtml()
+setError()
+aspectRatio
+height
+plotObject
+position
+status
+title
+width
+
+# container
+addCitation()
+dependOn()
+getError()
+print()
+printHtml()
+setError()
+position
+title
+
+# state
+dependOn()
+print()
+object
+
+# html
+addCitation()
+dependOn()
+getError()
+print()
+printHtml()
+setError()
+class
+elementType
+position
+text
+title
+
+# jaspResults
+addCitation()
+getError()
+print()
+printHtml()
+progressbarTick()
+setError()
+startProgressbar()
